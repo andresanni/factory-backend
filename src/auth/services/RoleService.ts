6473948314246cebe37853b7bbcb23b30ebb87ba
andresanni <andres.anni1985@gmail.com>
2024-10-09 10:35:37 -0300

    Removed ESLINT and PRETTIER configuration due to inconsistencies

--- src/auth/services/RoleService.ts
+++ src/auth/services/RoleService.ts
@@ -1,70 +1,86 @@
-import { RoleRepository } from "../repositories/RoleRepository";
-import { Role } from "../entities/Role";
-import { AuthenticatedUser } from "../types/AuthenticatedUser";
-import { Permissions } from "../types";
-import { PermissionRepository } from "../repositories/PermissionRepository";
-import { Permission } from "../entities/Permission";
-import { ErrorLayer, RepositoryLayerError } from "../../errors/AppError";
-import { handleError } from "../../utils/errorHandlerUtil";
+import { RoleRepository } from '../repositories/RoleRepository';
+import { Role } from '../entities/Role';
+import { AuthenticatedUser } from '../types/AuthenticatedUser';
+import { Permissions } from '../types';
+import { PermissionRepository } from '../repositories/PermissionRepository';
+import { Permission } from '../entities/Permission';
+import { ErrorLayer, RepositoryLayerError } from '../../errors/AppError';
+import { handleError } from '../../utils/errorHandlerUtil';
 
 export class RoleService {
   private roleRepository: RoleRepository;
   private permissionRepository: PermissionRepository;
 
-  constructor(roleRepository: RoleRepository, permissionRepository: PermissionRepository) {
+  constructor(
+    roleRepository: RoleRepository,
+    permissionRepository: PermissionRepository
+  ) {
     this.roleRepository = roleRepository;
     this.permissionRepository = permissionRepository;
   }
 
   //Utility method to check if the authenticated user has the required permission
-  private checkAuthorization(authenticatedUser: AuthenticatedUser, permission: Permissions, statusCode: {value: number} ): boolean {
-    const authorized: boolean = authenticatedUser.permissions.includes(permission);
+  private checkAuthorization(
+    authenticatedUser: AuthenticatedUser,
+    permission: Permissions,
+    statusCode: { value: number }
+  ): boolean {
+    const authorized: boolean =
+      authenticatedUser.permissions.includes(permission);
     if (!authorized) {
       statusCode.value = 403;
-      throw new Error(`Authorization error\n ${permission} isn't in user permissions. User permissions: ${authenticatedUser.permissions.length ? authenticatedUser.permissions.join(', ') : "empty"}`);
+      throw new Error(
+        `Authorization error\n ${permission} isn't in user permissions. User permissions: ${authenticatedUser.permissions.length ? authenticatedUser.permissions.join(', ') : 'empty'}`
+      );
     }
     return true;
   }
   //Utility method to resolve permissions from names to objects
-  private async resolvePermissions(permissionsNames: Permissions[], statusCode: {value: number}): Promise<Permission[]>{
-    const permissionsPromises = permissionsNames.map(
-      async(permissionName) => {
-        const permission = await this.permissionRepository.findByName(permissionName);
-        if(!permission) {
-          statusCode.value = 404;
-          throw new Error(`Permission ${permissionName} not found`);
-        }
-        return permission;
+  private async resolvePermissions(
+    permissionsNames: Permissions[],
+    statusCode: { value: number }
+  ): Promise<Permission[]> {
+    const permissionsPromises = permissionsNames.map(async (permissionName) => {
+      const permission =
+        await this.permissionRepository.findByName(permissionName);
+      if (!permission) {
+        statusCode.value = 404;
+        throw new Error(`Permission ${permissionName} not found`);
       }
-    );
+      return permission;
+    });
     const resolvedPermissions = await Promise.all(permissionsPromises);
     return resolvedPermissions;
   }
- 
+
   async createRole(
     authenticatedUser: AuthenticatedUser,
     roleName: string,
     permissionsNames?: Permissions[]
   ): Promise<Role> {
-    const statusCode: {value: number} = { value: 500 };
-    try {      
-      
-      this.checkAuthorization(authenticatedUser, Permissions.CREATE_ROLE, statusCode);
-      
-      const existingRole: Role | null = await this.roleRepository.findByName(
-        roleName
+    const statusCode: { value: number } = { value: 500 };
+    try {
+      this.checkAuthorization(
+        authenticatedUser,
+        Permissions.CREATE_ROLE,
+        statusCode
       );
+
+      const existingRole: Role | null =
+        await this.roleRepository.findByName(roleName);
 
       if (existingRole) {
         statusCode.value = 400;
-        throw new Error(`Role ${roleName} already exists`);
+        throw new Error('Role ${roleName} already exists');
       }
 
       const newRole = new Role(roleName);
 
-      
-      if (permissionsNames) {        
-        const resolvedPermissions = await this.resolvePermissions(permissionsNames, statusCode);        
+      if (permissionsNames) {
+        const resolvedPermissions = await this.resolvePermissions(
+          permissionsNames,
+          statusCode
+        );
         newRole.setPermissions(resolvedPermissions);
       }
 
@@ -76,8 +92,8 @@ export class RoleService {
       }
       if (error instanceof Error) {
         handleError(
-          "creating role",
-          "createRole()",
+          'creating role',
+          'createRole()',
           error,
           statusCode.value,
           ErrorLayer.SERVICE,
@@ -88,32 +104,39 @@ export class RoleService {
     }
   }
 
-   async assignPermissionsToRole(
+  async assignPermissionsToRole(
     authenticatedUser: AuthenticatedUser,
     roleName: string,
     permissionsNames: Permissions[]
   ): Promise<Role> {
-    const statusCode: {value: number} = {value: 500};
+    const statusCode: { value: number } = { value: 500 };
     try {
-      this.checkAuthorization(authenticatedUser, Permissions.UPDATE_ROLE, statusCode);
+      this.checkAuthorization(
+        authenticatedUser,
+        Permissions.UPDATE_ROLE,
+        statusCode
+      );
       const role: Role | null = await this.roleRepository.findByName(roleName);
 
       if (!role) {
         statusCode.value = 404;
         throw new Error(`Role ${roleName} not found`);
       }
-      const resolvedPermissions = await this.resolvePermissions(permissionsNames, statusCode);      
+      const resolvedPermissions = await this.resolvePermissions(
+        permissionsNames,
+        statusCode
+      );
       role.setPermissions(resolvedPermissions);
 
       if (role.id !== undefined) {
         const updatedRole = await this.roleRepository.update(role.id, role);
-        if(!updatedRole){
+        if (!updatedRole) {
           statusCode.value = 500;
           throw new Error(`Role ${roleName} couldn't be updated`);
         }
         return updatedRole;
       } else {
-        throw new Error("Role ID is undefined");
+        throw new Error('Role ID is undefined');
       }
     } catch (error) {
       if (error instanceof RepositoryLayerError) {
@@ -121,8 +144,8 @@ export class RoleService {
       }
       if (error instanceof Error) {
         handleError(
-          "assigning permissions to role",
-          "assignPermissionsToRole()",
+          'assigning permissions to role',
+          'assignPermissionsToRole()',
           error,
           statusCode.value,
           ErrorLayer.SERVICE,
@@ -132,4 +155,4 @@ export class RoleService {
       throw error;
     }
   }
-} 
+}
